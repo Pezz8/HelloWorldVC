9491f795888f101dac94849fdef37fae4e2bbfb5
Pezz8 <43047594+Pezz8@users.noreply.github.com>
2022-02-24 02:37:42 -0500

    Updated Test File

--- src/HelloWorldVC.java
+++ src/HelloWorldVC.java
@@ -1,6 +1,10 @@
 
 public class HelloWorldVC{
     public static void main(String[] args) {
+        //English
         System.out.println("Hello World");
+        //Italian
+        System.out.println("Ciao Mondo");
+        //Spanish
     }
 }
